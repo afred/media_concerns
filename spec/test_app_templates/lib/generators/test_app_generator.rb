5af447e41f9f6e8bb381b16d5362eb4ff68bed51
Andrew Myers <afredmyers@gmail.com>
2016-08-07 18:47:38 -0600

    Fix indentation.

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,18 +1,18 @@
-        require 'rails/generators'
+require 'rails/generators'
 
-        class TestAppGenerator < Rails::Generators::Base
-          source_root "./spec/test_app_templates"
+class TestAppGenerator < Rails::Generators::Base
+  source_root "./spec/test_app_templates"
 
-          def install_curation_concerns
-            generate "curation_concerns:install -f"
-          end
+  def install_curation_concerns
+    generate "curation_concerns:install -f"
+  end
 
-          # if you need to generate any additional configuration
-          # into the test app, this generator will be run immediately
-          # after setting up the application
+  # if you need to generate any additional configuration
+  # into the test app, this generator will be run immediately
+  # after setting up the application
 
-          def install_engine
-            generate 'media_concerns:install'
-          end
-        end
+  def install_engine
+    generate 'media_concerns:install'
+  end
+end
 
